31250253b2ef262a964f044250f379b58166d6c3
Adam Kurzawa <adam.kurzawa.70@gmail.com>
2024-03-29 00:26:05 +0100

    Added welcome

---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Panel from "./Panel.vue";
+</script>
+
+<template>
+  <Panel class="font-yeseva welcome" max-width="750px">
+    Witam na mojej stronie. Nazywam się Adam Kurzawa i zapraszam Cię do
+    spędzenia kilku chwil pośród opowiadań mojego autorstwa.
+  </Panel>
+</template>
+
+<style scoped>
+.welcome {
+  font-size: 1.25rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
+import Welcome from "@/components/Welcome.vue";
 import BookAd from "../components/BookAd.vue";
 </script>
 
 <template>
   <main>
+    <div class="welcome-container">
+      <Welcome/>
+    </div>
     <BookAd />
   </main>
 </template>
+
+<style scoped>
+.welcome-container {
+  display: flex; 
+  justify-content: center;
+  margin-bottom: 2rem;
+}
+</style>
