612f929cbace0dc804d38fb7d12edd47acb41b85
Adam Kurzawa <adam.kurzawa.70@gmail.com>
2024-03-29 22:00:20 +0100

    Added gtag

--- src/main.js
+++ src/main.js
@@ -3,9 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from "vue-gtag";
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(VueGtag, {
+    config: {
+        id: "G-6Z9LBMVFVM",
+        params: {
+            anonymize_ip: true
+        }
+    }
+})
 
 app.mount('#app')
