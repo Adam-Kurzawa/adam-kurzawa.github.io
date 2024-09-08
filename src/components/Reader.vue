b62897e6f88c1df0a7a8b32b4350ad5661dd740d
Adam Kurzawa <adam.kurzawa.70@gmail.com>
2024-09-09 01:19:49 +0200

    Better reader

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -12,7 +12,7 @@ const router = useRouter()
 const route = useRoute()
 const t = useTranslation()
 
-const darkMode = ref(true);
+const darkMode = ref('dark');
 const fontSize = ref(1.25);
 const fontFamily = ref('Times New Roman');
 
@@ -22,9 +22,9 @@ const charactersCount = computed(() => props.story.chapters.reduce(
   0,
 ))
 
-const semitransparentBgClass = computed(() => darkMode.value ? 'dark-mode-semitransparent-bg' : 'light-mode-semitransparent-bg')
-const solidBgClass = computed(() => darkMode.value ? 'dark-mode-bg' : 'light-mode-bg')
-const textClass = computed(() => darkMode.value ? 'dark-mode-text' : 'light-mode-text')
+const semitransparentBgClass = computed(() => `${darkMode.value}-mode-semitransparent-bg`)
+const solidBgClass = computed(() => `${darkMode.value}-mode-bg`)
+const textClass = computed(() => `${darkMode.value}-mode-text`)
 
 const saveAsPdf = () => PdfService.saveAsPdf(props.story.title, props.story.chapters)
 const saveAsEpub = () => EpubService.saveAsEpub(props.story.title, props.story.chapters, t("reader.epub-chapter"))
@@ -38,46 +38,36 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
 
 <template>
   <main>
-    <Panel rounded="false" class="float" :class="[semitransparentBgClass]">
+    <Panel rounded="false" class="float border" :class="[semitransparentBgClass]">
       <div class="settings">
-        <div class="small-controls">
-          <div class="btn-group dropdown">
-            <p
-              :class="[textClass]"
-              style="
-                border-top-right-radius: 0.5rem !important;
-                border-bottom-right-radius: 0.5rem !important;
-                border-right: 1px #e69b54 solid;
-              "
-            >
-              {{ t("reader.download") }}
-            </p>
-            <div class="dropdown-content" :class="[solidBgClass]">
-              <a href="#" class="font-segoe" :class="[textClass]" @click="saveAsPdf">PDF</a>
-              <a href="#" class="font-segoe" :class="[textClass]" @click="saveAsEpub">ePUB</a>
-              <!-- <a href="#" class="font-segoe" :class="[textClass]">Mobi</a> -->
-              <!-- <a href="#" class="font-segoe" :class="[textClass]">AZW3</a> -->
-            </div>
-          </div>
-          <div class="btn-group">
-            <p :class="[textClass]">{{ t("reader.theme") }}</p>
-            <button @click="() => { darkMode = !darkMode; }" :class="[textClass]">
-              {{ darkMode ? t("reader.light") : t("reader.dark") }}
-            </button>
-          </div>
+        <div class="btn-group">
+          <p :class="[textClass]">{{ t("reader.download") }}</p>
+          <button href="#" class="font-segoe" :class="[textClass]" @click="saveAsPdf">MOBI</button>
+          <button href="#" class="font-segoe" :class="[textClass]" @click="saveAsEpub">ePUB</button>
+          <button href="#" class="font-segoe" :class="[textClass]" @click="saveAsEpub">{{ t("reader.sendToKindle") }}</button>
         </div>
         <div class="btn-group">
-          <p :class="[textClass]">{{ t("reader.fontSize") }}</p>
-          <button @click="() => { fontSize = fontSize + 0.25; }" :class="[textClass]">+</button>
-          <button @click="() => { fontSize = 1.25; }" :class="[textClass]">100%</button>
-          <button @click="() => { fontSize = fontSize - 0.25; }" :class="[textClass]">-</button>
+          <p :class="[textClass]">{{ t("reader.theme") }}</p>
+          <button @click="() => { darkMode = 'light'; }" :class="[textClass]">{{ t("reader.light") }}</button>
+          <button @click="() => { darkMode = 'sepia'; }" :class="[textClass]">{{ t("reader.sepia") }}</button>
+          <button @click="() => { darkMode = 'dark'; }" :class="[textClass]">{{ t("reader.dark") }}</button>
         </div>
         <div class="btn-group">
           <p :class="[textClass]">{{ t("reader.font") }}</p>
-          <button @click="() => { fontFamily = 'Times New Roman'; }" :class="[textClass]">Times</button>
-          <button @click="() => { fontFamily = 'Georgia'; }" :class="[textClass]">Georgia</button>
-          <button @click="() => { fontFamily = 'Arial'; }" :class="[textClass]">Arial</button>
-          <button @click="() => { fontFamily = 'Verdana'; }" :class="[textClass]">Verdana</button>
+          <span :class="[textClass]">{{ fontSize * 10 }}</span>
+          <select :class="[textClass]" v-model="fontFamily">
+            <option :class="[solidBgClass]" style="font-family: 'Times New Roman';">Times New Roman</option>
+            <option :class="[solidBgClass]" style="font-family: Georgia;">Georgia</option>
+            <option :class="[solidBgClass]" style="font-family: Arial;">Arial</option>
+            <option :class="[solidBgClass]" style="font-family: Verdana;">Verdana</option>
+            <option :class="[solidBgClass]" style="font-family: 'Segoe UI';">Segoe UI</option>
+            <option :class="[solidBgClass]" style="font-family: 'Josefin Sans';">Josefin Sans</option>
+            <option :class="[solidBgClass]" style="font-family: 'Yeseva One';">Yeseva One</option>
+            <option :class="[solidBgClass]" style="font-family: 'Madimi One';">Madimi One</option>
+          </select>
+          <button @click="() => { fontSize = fontSize + 0.25; }" :class="[textClass]">+</button>
+          <button @click="() => { fontSize = 1.25; }" :class="[textClass]">100%</button>
+          <button @click="() => { fontSize = fontSize - 0.25; }" :class="[textClass]">-</button>
         </div>
       </div>
     </Panel>
@@ -103,38 +93,8 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
 </template>
 
 <style scoped>
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  border: 1px #e69b54 solid;
-  border-radius: 0.5rem;
-  top: -3.5rem;
-  min-width: 160px;
-  z-index: 1;
-}
-
-.dropdown-content a {
-  padding: 12px 16px;
-  border-radius: 0.5rem;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown-content a:hover {
-  background-color: #e69b54;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-.dropdown:hover .dropbtn {
-  background-color: #3e8e41;
+.border {
+  border-top: 1px solid gray;
 }
 
 .dark-mode-bg {
@@ -145,12 +105,20 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
   background-color: white;
 }
 
+.sepia-mode-bg {
+  background-color: rgb(236, 228, 202);
+}
+
 .dark-mode-semitransparent-bg {
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(30, 54, 54, 0.65);
 }
 
 .light-mode-semitransparent-bg {
-  background-color: rgba(255, 255, 255, 0.5);
+  background-color: rgba(255, 255, 255, 0.65);
+}
+
+.sepia-mode-semitransparent-bg {
+  background-color: rgba(236, 228, 202, 0.65);
 }
 
 .dark-mode-text {
@@ -158,6 +126,10 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
 }
 
 .light-mode-text {
+  color: black;
+}
+
+.sepia-mode-text {
   color: black;
 }
 
@@ -196,12 +168,22 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 
-.btn-group select {
+.btn-group span {
   transition: font-size 1s ease;
   border: 1px solid #e69b54;
   padding: 0.5rem 1rem;
   float: left;
-  font-weight: bold;
+  margin: 0;
+  font-size: 1rem;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.btn-group select {
+  transition: font-size 1s ease;
+  border: 1px solid #e69b54;
+  padding: 0.45rem 1rem;
+  float: left;
+  outline: none;
   margin: 0;
   background: none;
   font-size: 1rem;
@@ -249,10 +231,6 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
   backdrop-filter: blur(10px);
 }
 
-.small-controls {
-  display: contents;
-}
-
 .settings {
   display: flex;
   gap: 2rem;
@@ -296,7 +274,32 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
   margin-bottom: 10rem;
 }
 
-@media screen and (max-width: 1200px) {
+@media screen and (max-width: 1600px) {
+  .btn-group button {
+    font-size: 0.90rem;
+  }
+
+  .btn-group p {
+    font-size: 0.90rem;
+  }
+
+  .btn-group select {
+    font-size: 0.90rem;
+  }
+  
+  .btn-group span {
+    font-size: 0.90rem;
+  }
+}
+
+@media screen and (max-width: 1440px) {
+  .settings {
+    gap: 1rem;
+    flex-wrap: wrap;
+  }
+}
+
+@media screen and (max-width: 1280px) {
   .btn-group button {
     font-size: 0.75rem;
   }
@@ -306,39 +309,18 @@ const nextPageDisabled = computed(() => props.chapter === props.story.chapters.l
   }
 
   .btn-group select {
+    font-size: 0.75rem;
+  }
+  
+  .btn-group span {
     font-size: 0.75rem;
   }
 }
 
 @media screen and (max-width: 1024px) {
-  .settings {
-    display: block;
-  }
-  
-  .btn-group {
-    margin-bottom: 0.5rem;
-  }
-
-  .btn-group button {
-    font-size: 0.75rem;
-  }
-
-  .btn-group p {
-    font-size: 0.75rem;
-  }
-
-  .btn-group select {
-    font-size: 0.75rem;
-  }
-
   .float {
-    padding-bottom: 0 !important;
+    padding-bottom: 1rem !important;
     padding-top: 1rem !important;
-  }
-
-  .small-controls {
-    display: flex;
-    gap: 1rem;
   }
 }
 </style>
