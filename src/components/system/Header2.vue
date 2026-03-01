b077ededb09e53a7ad82641e1b758c807e7e1c00
Adam Kurzawa <adam.kurzawa.70@gmail.com>
2026-03-02 00:06:35 +0100

    WIP5

--- src/components/system/Header2.vue
+++ src/components/system/Header2.vue
@@ -6,7 +6,7 @@ const props = defineProps([ 'value' ])
 
 <template>
     <div>
-        <h2 class="text-3xl font-serif text-slate-900">{{ props.value }}</h2>
+        <h2 class="text-3xl font-serif text-slate-900 mb-2">{{ props.value }}</h2>
         <HeaderUnderline />
     </div>
 </template>
